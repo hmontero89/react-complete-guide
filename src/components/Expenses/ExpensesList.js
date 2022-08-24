958d3338a0238df03f49a1ad9457700c90e799ff
Hebert Montero García <hebertenrique.montero@bbva.com>
2022-08-24 02:01:48 +0200

    Add logic to create new expense and filter expenses

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -8,34 +8,24 @@ const ExpensesList = (props) => {
     const expenses = props.expenses;
     const [selectedFilter, setSelectedFilter] = useState('2020');
     const filterChangeHandler = (filter) => {
-        console.log('filter', filter);
-        setSelectedFilter(selectedFilter);
+        setSelectedFilter(filter);
     };
+    const filteredExpenses = expenses.filter((expense) => {
+        return expense.date.getFullYear().toString() === selectedFilter;
+    })
 
     return (
         <div className="expenses">
             <ExpensesFilter selected={selectedFilter} onFilterChanged={filterChangeHandler} />
             <Card>
-                <ExpenseItem
-                    title={expenses[0].title}
-                    amount={expenses[0].amount}
-                    date={expenses[0].date}
-                />
-                <ExpenseItem
-                    title={expenses[1].title}
-                    amount={expenses[1].amount}
-                    date={expenses[1].date}
-                />
-                <ExpenseItem
-                    title={expenses[2].title}
-                    amount={expenses[2].amount}
-                    date={expenses[2].date}
-                />
-                <ExpenseItem
-                    title={expenses[3].title}
-                    amount={expenses[3].amount}
-                    date={expenses[3].date}
-                />
+                {filteredExpenses.map((expense) => (
+                    <ExpenseItem
+                        key={expense.id}
+                        title={expense.title}
+                        amount={expense.amount}
+                        date={expense.date}
+                    />
+                ))}
             </Card>
         </div>
     );
